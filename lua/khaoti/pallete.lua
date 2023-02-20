216180d7ea90cb795bd94934c6d2f5f6c58658e9
emptybrain <empbrain@proton.me>
2023-02-20 12:21:13 -0300

    update syntax colors

--- lua/khaoti/pallete.lua
+++ lua/khaoti/pallete.lua
@@ -1,50 +1,56 @@
 local hsl = require("lush").hsl
 local colors = {
-  bg = hsl("#121212"),
-  background = hsl("#000000"),
-  float_bg = hsl("#1c1c1c"),
-  float_bg2 = hsl("#25252a"),
-  fg = hsl("#D4D4D4"),
-  fg2 = hsl("#6f6f6f"),
-  --
-  red = hsl("#E57373"),
-  red2 = hsl("#EF5350"),
-  red3 = hsl("#E53935"),
-  --
-  orange = hsl("#FFB74D"),
-  orange2 = hsl("#FF9800"),
-  --
-  yellow = hsl("#FFEE58"),
-  yellow2 = hsl("#FDD835"),
-  --
-  green = hsl("#AED581"),
-  green2 = hsl("#81C784"),
-  lime = hsl("#CDDC39"),
-  --
-  cyan = hsl("#80b4bb"),
-  cyan2 = hsl("#0097A7"),
-  --
-  blue = hsl("#64B5F6"),
-  blue2 = hsl("#2196F3"),
-  --
-  purple = hsl("#9575CD"),
-  --
-  white = hsl("#abb2bf"),
-  --
-  black = hsl("#282c34"),
-  black2 = hsl("#333842"),
-  --
-  grey = hsl("#757575"),
-  dark_grey = hsl("#424242"),
-  
-  --
-  brown = hsl("#A1887F"),
-  --
-  none = "NONE",
-  --
-  cursoline_bg = hsl("#222222"),
-  --
-  string_fg = hsl("#787f68"),
+	bg = hsl("#121212"),
+	background = hsl("#151515"),
+    background2 = hsl("#0d0d0d"),
+	background_black = hsl("#000000"),
+	float_bg = hsl("#1c1c1c"),
+	float_bg2 = hsl("#25252a"),
+	fg = hsl("#d4d4d4"),
+	fg2 = hsl("#6f6f6f"),
+	--
+	red = hsl("#E57373"),
+	red2 = hsl("#EF5350"),
+	red3 = hsl("#E53935"),
+	--
+	orange = hsl("#ffcb6b"),
+	orange2 = hsl("#ff9800"),
+	--
+	yellow = hsl("#fff27f"),
+	yellow2 = hsl("#fdd835"),
+	--
+	green = hsl("#aed581"),
+	green2 = hsl("#81c784"),
+	lime = hsl("#cddc39"),
+	--
+	cyan = hsl("#80b4bb"),
+	cyan2 = hsl("#0097a7"),
+	cyan3 = hsl("#88afa2"),
+	--
+	blue = hsl("#64b5f6"),
+	blue2 = hsl("#2196f3"),
+    blue3 = hsl("#89b4fa"),
+	--
+	purple = hsl("#9575cd"),
+    purple2 = hsl("#8576a0"),
+	--
+	white = hsl("#d4d4d4"),
+	--
+	black = hsl("#282c34"),
+	black2 = hsl("#333842"),
+	--
+	grey = hsl("#757575"),
+	dark_grey = hsl("#424242"),
+	--
+	brown = hsl("#A1887F"),
+	--
+	none = "NONE",
+	--
+	cursoline_bg = hsl("#151515"),
+	--
+	string_fg = hsl("#787f68"),
+    --
+    visual_select = hsl("#242424"),
 }
 
 return colors
